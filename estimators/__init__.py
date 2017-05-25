237b8d0cf2cd830345576b189eb2d553bd6cbdb7
Florin Gogianu <florin.gogianu@gmail.com>
2017-05-25 15:41:20 +0300

    Remove mini_net model.

--- estimators/__init__.py
+++ estimators/__init__.py
@@ -1,14 +1,12 @@
 # Bitdefender, 2107
 
 from .atari_conv import AtariConv
-from .mini_net import MiniNet
 from .catch_net import CatchNet
 from .catch_conv import CatchConv
 from .kernel_density_estimate import KernelDensityEstimate
 
 ESTIMATORS = {
     "atari_conv": AtariConv,
-    "mini": MiniNet,
     "catch": CatchNet,
     "catch_conv": CatchConv
 }
